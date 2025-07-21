24000c1e5a97185f42262f3670d6f992f0f9ec9f
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-07-21 12:06:50 +0300

    adding gridTemplateColumns

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         extend: {
            fontFamily: {
             'sans': ['Poppins', 'sans-serif'],
+           },
+           gridTemplateColumns: {
+            '70/30': '70% 28%'
            }
         },
     },
