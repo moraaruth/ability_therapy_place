45b75b5c203a59b4383ba14ebca5dbf4d3de3295
Ruth Moraa Nyangau <rmnyangau@safaricom.co.ke>
2025-07-21 11:59:15 +0300

    adding poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
     ],
     theme: {
         extend: {
-           
+           fontFamily: {
+            'sans': ['Poppins', 'sans-serif'],
+           }
         },
     },
     plugins: [require("daisyui")],
